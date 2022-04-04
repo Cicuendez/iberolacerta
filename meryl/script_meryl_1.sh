69b1b16f4edf9ade5f08a410ac4368ad5bcc8361
Hector Tejero Cicuendez <47630372+Cicuendez@users.noreply.github.com>
2022-04-04 18:02:44 +0200

    Update script_meryl_1.sh

--- meryl/script_meryl_1.sh
+++ meryl/script_meryl_1.sh
@@ -1,3 +1,6 @@
 # meryl count k=21 datafile.fastq.gz output output_folder_path
 
-meryl count k=21 /home/panthera/DiscoD/iberolacerta/data/m64072_220317_150612.hifi_reads.fastq.gz output /home/panthera/DiscoD/iberolacerta/meryl
+meryl count k=21 /home/panthera/DiscoD/iberolacerta/data/m64072_220317_150612.hifi_reads.fastq.gz output /home/panthera/DiscoD/iberolacerta/meryl/hifi1
+meryl count k=21 /home/panthera/DiscoD/iberolacerta/data/m64072_220331_151127.hifi_reads.fastq.gz output /home/panthera/DiscoD/iberolacerta/meryl/hifi2
+
+# do this for each file, in this case two files, and individual folders will be created with the outputs: hifi1 and hifi2
